4d779c3b2650032ef94657d21bf9c6a18ecc24b9
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-07-07 22:00:31 -0500

    adding style js

---
+++ styles.js
@@ -0,0 +1,16 @@
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.onClick = () => {
+    navigation.classList.toggle('open');
+}
+const listItems = document.querySelectorAll('.list-item');
+listItems.forEach(item => {
+    item.onClick = () => {
+        listItems.forEach( item =>
+            item.classList.remove('active')
+        );
+        item.classList.add('active');
+    }
+})
