727711cc1923c294033bc26be322aab41a7f5b9c
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-07-08 12:36:49 -0500

    adding style css changes

--- styles.js
+++ styles.js
@@ -1,16 +1,21 @@
-
+document.getElementById("li-home").style.color = "#e94848";
+document.getElementById("li-about").style.color = "#459b37";
+document.getElementById("li-contact").style.color = "#964cb0";
+document.getElementById("li-portfolio").style.color = "#efbc45";
+document.getElementById("li-blog").style.color = "#3b48db";
 
 const menuToggle = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation');
-menuToggle.onClick = () => {
-    navigation.classList.toggle('open');
-}
+menuToggle.onClick = navigation.classList.toggle('open');
+
 const listItems = document.querySelectorAll('.list-item');
-listItems.forEach(item => {
-    item.onClick = () => {
-        listItems.forEach( item =>
-            item.classList.remove('active')
-        );
-        item.classList.add('active');
+listItems.forEach(
+    item => {
+        item.onClick = () => {
+            listItems.forEach( 
+                item => item.classList.remove('active')
+            );
+            item.classList.add('active');
+        }
     }
-})
+)
